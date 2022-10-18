847941d8e8b4d354e9d0375bca13309c80bbe224
heyitsbec <103087533+heyitsbec@users.noreply.github.com>
2022-10-17 19:24:37 -0700

    completed lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Rebecca Slason
+* Created: 17 October 2022
+* Lisence: Public Domain
+*/
+
+// Define Variables
+make = "Chevy";
+model = "Cruze";
+color = "White";
+year = 2017;
+
+//calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
